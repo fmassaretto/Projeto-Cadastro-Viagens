558548a7990d1d7cfaa44f25cd10455700d94ea7
fmassaretto <fabio_no_more@yahoo.com.br>
2017-04-09 21:18:58 -0300

    Add interface 'IClientes' and other classes

---
+++ ProjetoViagens/ProjetoViagens/Model/Interface/ICliente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoViagens.Model.Interface
+{
+    interface ICliente
+    {
+        int Id { get; set; }
+        string Nome { get; set; }
+        string Documento { get; set; }
+    }
+}
