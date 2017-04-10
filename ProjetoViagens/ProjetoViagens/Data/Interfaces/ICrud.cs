05d7a4b81fe1482d3f7464567f76f7382f391333
fmassaretto <fabio_no_more@yahoo.com.br>
2017-04-09 21:17:04 -0300

    Add Interface 'ICrud' and class 'Repository'

---
+++ ProjetoViagens/ProjetoViagens/Data/Interfaces/ICrud.cs
@@ -0,0 +1,15 @@
+﻿using ProjetoViagens.DB.Base;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoViagens.Data.Interfaces
+{
+    public interface ICrud
+    {
+        SqlDataReader ExecuteProc(string query, List<SqlParameter> parametros);
+    }
+}
---
+++ ProjetoViagens/ProjetoViagens/Data/Repository.cs
@@ -0,0 +1,47 @@
+﻿using ProjetoViagens.Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using ProjetoViagens.DB.Base;
+
+namespace ProjetoViagens.Data
+{
+    class Repository<T> : Connection, ICrud
+    {
+        public SqlDataReader ExecuteProc(string query, List<SqlParameter> parametros)
+        {
+            SqlCommand comando = new SqlCommand();
+            Connection conexao = new Connection();
+            comando.CommandType = System.Data.CommandType.StoredProcedure;
+            comando.CommandText = query;
+
+            foreach (var item in parametros)
+            {
+                comando.Parameters.Add(item);
+            }
+            comando.Connection = conexao.GetConnection();
+            comando.Connection.Open();
+            SqlDataReader reader = comando.ExecuteReader();
+
+            return reader;
+        }
+
+        public SqlDataReader ExecuteProc(string query)
+        {
+            SqlCommand comando = new SqlCommand();
+            Connection conexao = new Connection();
+            comando.CommandType = System.Data.CommandType.StoredProcedure;
+            comando.CommandText = query;
+
+            comando.Connection = conexao.GetConnection();
+            comando.Connection.Open();
+            SqlDataReader reader = comando.ExecuteReader();
+
+            return reader;
+        }
+
+    }
+}
