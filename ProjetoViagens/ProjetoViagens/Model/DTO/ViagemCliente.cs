355bdc9f2607d7e937e12d34df84aa07a3b31a75
fmassaretto <fabio_no_more@yahoo.com.br>
2017-04-10 23:38:49 -0300

    Created class 'ViagemCliente' and added properties

---
+++ ProjetoViagens/ProjetoViagens/Model/DTO/ViagemCliente.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoViagens.Model.DTO
+{
+    class ViagemCliente : ViagensDispo
+    {
+        public int IdCliente { get; set; }
+        public int IdViagemDispo { get; set; }
+    }
+}
