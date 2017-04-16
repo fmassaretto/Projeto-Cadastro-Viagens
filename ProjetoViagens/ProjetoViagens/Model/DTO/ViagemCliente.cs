a22d9376210848904c6d205e1f68ce66c9566fe6
fmassaretto <fabio_no_more@yahoo.com.br>
2017-04-16 00:27:44 -0300

    Added 'Transporte' class and properties to constructor

--- ProjetoViagens/ProjetoViagens/Model/DTO/ViagemCliente.cs
+++ ProjetoViagens/ProjetoViagens/Model/DTO/ViagemCliente.cs
@@ -6,27 +6,33 @@ using System.Threading.Tasks;
 
 namespace ProjetoViagens.Model.DTO
 {
-    class ViagemCliente : ViagensDispo
+    public class ViagemCliente
     {
         public int CodigoReserva { get; set; }
         public int IdCliente { get; set; }
         public int IdViagemDispo { get; set; }
+        public int IdTransporte { get; set; }
 
         public Clientes clientes = new Clientes();
 
-        public ViagemCliente(int idCliente, string nome, string documento, bool respira, int codigoReserva, string planetaOrigem, string planetaDestino, int valor, int tempo)
+        public ViagensDispo viagemDispo = new ViagensDispo();
+
+        public Transportes transportes = new Transportes();
+
+        public ViagemCliente(int idCliente, string nome, string documento, bool respira, int codigoReserva, string planetaOrigem, string planetaDestino, int valor, int tempo, string nomeTransporte, string terreno)
         {
-            //clientes.Id = idCliente;
+            clientes.Id = idCliente;
             clientes.Nome = nome;
             clientes.Documento = documento;
             clientes.Respira = respira;
             this.IdCliente = idCliente;
-            this.Id = codigoReserva;
-            base.PlanetaOrigem = planetaOrigem;
-            base.PlanetaDestino = planetaDestino;
-            base.Valor = valor;
-            base.Tempo = tempo;
-
+            this.CodigoReserva = codigoReserva;
+            viagemDispo.PlanetaOrigem = planetaOrigem;
+            viagemDispo.PlanetaDestino = planetaDestino;
+            viagemDispo.Valor = valor;
+            viagemDispo.Tempo = tempo;
+            transportes.Nome = nomeTransporte;
+            transportes.Terreno = terreno;
 
         }
 
